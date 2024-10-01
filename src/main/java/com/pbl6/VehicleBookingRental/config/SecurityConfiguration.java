dcdc09cd4aea3320e7314b6236043c08e3215e2f
QuyHoang7703 <hoangvanquy772003@gmail.com>
2024-10-01 20:05:09 +0700

    add cors config

--- src/main/java/com/pbl6/VehicleBookingRental/config/SecurityConfiguration.java
+++ src/main/java/com/pbl6/VehicleBookingRental/config/SecurityConfiguration.java
@@ -36,6 +36,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http, CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
         http
                 .csrf(c->c.disable())
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(
                         authz -> authz
                                 .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh").permitAll()

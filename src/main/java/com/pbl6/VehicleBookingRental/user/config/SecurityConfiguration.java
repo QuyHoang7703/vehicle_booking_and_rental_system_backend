11d203a52507a022303571d79757587903e76445
QuyHoang7703 <hoangvanquy772003@gmail.com>
2024-10-07 11:09:28 +0700

    login with account google

--- src/main/java/com/pbl6/VehicleBookingRental/user/config/SecurityConfiguration.java
+++ src/main/java/com/pbl6/VehicleBookingRental/user/config/SecurityConfiguration.java
@@ -63,14 +63,14 @@ public class SecurityConfiguration {
                                                 "/api/v1/auth/resend_otp", "/api/v1/auth/register-info", "/api/v1/auth/google-login").permitAll()
                                 .anyRequest().authenticated()
                                 )
-                // .oauth2Login(oauth2 -> oauth2
-                //     .loginPage("/api/v1/auth/google-login")  // Chỉ định trang login cho OAuth2
-                //     .authorizationEndpoint(authorization -> authorization
-                //         .baseUri("/api/v1/auth/google-login")  // Đường dẫn cho OAuth2 login
-                //     )
-                //     .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService)) // Xử lý thông tin người dùng
-                //     .successHandler(customAuthenticationSuccessHandler)  // Xử lý khi đăng nhập thành công
-                // )
+                .oauth2Login(oauth2 -> oauth2
+                    .loginPage("/api/v1/auth/google-login")  // Chỉ định trang login cho OAuth2
+                    .authorizationEndpoint(authorization -> authorization
+                        .baseUri("/api/v1/auth/google-login")  // Đường dẫn cho OAuth2 login
+                    )
+                    .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService)) // Xử lý thông tin người dùng
+                    .successHandler(customAuthenticationSuccessHandler)  // Xử lý khi đăng nhập thành công
+                )
                                 
                                 
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())

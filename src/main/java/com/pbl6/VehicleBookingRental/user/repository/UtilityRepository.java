714f25b234daab00d4a97456c50b6112d6f98766
QuyHoang7703 <hoangvanquy772003@gmail.com>
2024-11-02 09:28:13 +0700

    create api for utilities

--- src/main/java/com/pbl6/VehicleBookingRental/user/repository/UtilityRepository.java
+++ src/main/java/com/pbl6/VehicleBookingRental/user/repository/UtilityRepository.java
@@ -1,8 +1,11 @@
-package com.pbl6.VehicleBookingRental.user.repository.busPartner;
+package com.pbl6.VehicleBookingRental.user.repository;
 
 import com.pbl6.VehicleBookingRental.user.domain.bus_service.Utility;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface UtilityRepository extends JpaRepository<Utility, Integer>, JpaSpecificationExecutor<Utility> {
+    Optional<Utility> findByName(String name);
 }
--- src/main/java/com/pbl6/VehicleBookingRental/user/service/UtilityService.java
+++ src/main/java/com/pbl6/VehicleBookingRental/user/service/UtilityService.java
@@ -5,12 +5,13 @@ import com.pbl6.VehicleBookingRental.user.dto.ResultPaginationDTO;
 import com.pbl6.VehicleBookingRental.user.util.error.ApplicationException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface UtilityService {
-    Utility createUtility(Utility reqUtility) throws ApplicationException;
-    Utility updateUtility(Utility reqUtility);
-    void deleteUtility(int idUtility);
-    Utility getUtility(int idUtility);
+    Utility createUtility(Utility reqUtility, MultipartFile utilityImage) throws ApplicationException;
+    Utility updateUtility(Utility reqUtility, MultipartFile utilityImage) throws ApplicationException;
+    void deleteUtility(int idUtility) throws ApplicationException;
+    Utility getUtility(int idUtility) throws ApplicationException;
     ResultPaginationDTO getAllUtility(Specification<Utility> specification, Pageable pageable);
 
 }

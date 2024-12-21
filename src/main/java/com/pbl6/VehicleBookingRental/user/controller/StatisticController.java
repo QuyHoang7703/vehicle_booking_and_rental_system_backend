d0bba1434284c417b9e3a3a7e4119ca8937bcb7a
QuyHoang7703 <hoangvanquy772003@gmail.com>
2024-12-21 20:27:58 +0700

    check cloudinary

--- src/main/java/com/pbl6/VehicleBookingRental/user/controller/StatisticController.java
+++ src/main/java/com/pbl6/VehicleBookingRental/user/controller/StatisticController.java
@@ -2,12 +2,17 @@ package com.pbl6.VehicleBookingRental.user.controller;
 
 import com.pbl6.VehicleBookingRental.user.service.statistic.StatisticService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class StatisticController {
     private final StatisticService statisticService;
+
+//    @GetMapping("")
 }

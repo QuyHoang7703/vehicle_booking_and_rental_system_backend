3b3bfe406530b6190b78626b523c0f602fe35aac
nmquang30102003 <nmquang30102003@gmail.com>
2024-12-24 22:14:00 +0700

    refactor/update-statistic-vehicle-rental-order

--- src/main/java/com/pbl6/VehicleBookingRental/user/controller/VehicleRentalStatisticsController.java
+++ src/main/java/com/pbl6/VehicleBookingRental/user/controller/VehicleRentalStatisticsController.java
@@ -19,23 +19,34 @@ public class VehicleRentalStatisticsController {
     @Autowired
     private VehicleRentalStatisticService vehicleRentalStatisticService;
     @GetMapping("/statistic-from-location-or-vehicleType-by-date")
-    public ResponseEntity<?> statisticByLocationOrVehicleTypeDate(@RequestParam(required = false,value = "location")String location,
-                                                              @RequestParam(required = false,value = "vehicle_type")String vehicleTypeName,
-                                                              @RequestParam("start_date") String startDate,@RequestParam("end_date")String endDate){
-        return ResponseEntity.status(HttpStatus.OK).body(vehicleRentalStatisticService.statisticFromLocationOrVehicleTypeByDate(location,vehicleTypeName,startDate,endDate));
+    public ResponseEntity<?> statisticByLocationOrVehicleTypeDate(
+            @RequestParam(required = false, value = "location") String location,
+            @RequestParam(required = false, value = "vehicle_type") String vehicleTypeName,
+            @RequestParam("start_date") String startDate,
+            @RequestParam("end_date") String endDate) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                vehicleRentalStatisticService.statisticFromLocationOrVehicleTypeByDate(location, vehicleTypeName, startDate, endDate));
     }
+
     @GetMapping("/statistic-from-location-or-vehicleType-by-month-and-year")
-    public ResponseEntity<?> statisticByLocationOrVehicleTypeMonthAndYear(@RequestParam(required = false,value = "location")String location,
-                                                              @RequestParam(required = false,value = "vehicle_type")String vehicleTypeName,
-                                                              @RequestParam("month") int month,@RequestParam("end_date")int year){
-        return ResponseEntity.status(HttpStatus.OK).body(vehicleRentalStatisticService.statisticFromLocationOrVehicleTypeByMonthAndYear(location,vehicleTypeName,month,year));
+    public ResponseEntity<?> statisticByLocationOrVehicleTypeMonthAndYear(
+            @RequestParam(required = false, value = "location") String location,
+            @RequestParam(required = false, value = "vehicle_type") String vehicleTypeName,
+            @RequestParam("month") int month,
+            @RequestParam("year") int year) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                vehicleRentalStatisticService.statisticFromLocationOrVehicleTypeByMonthAndYear(location, vehicleTypeName, month, year));
     }
-    @GetMapping("/statistic-from-location-or-vehicleType-by-date")
-    public ResponseEntity<?> statisticByLocationOrVehicleTypeYear(@RequestParam(required = false,value = "location")String location,
-                                                              @RequestParam(required = false,value = "vehicle_type")String vehicleTypeName,
-                                                              @RequestParam("year") List<Integer> years) throws ApplicationException {
-        return ResponseEntity.status(HttpStatus.OK).body(vehicleRentalStatisticService.statisticFromLocationOrVehicleTypeByYear(location,vehicleTypeName,years));
+
+    @GetMapping("/statistic-from-location-or-vehicleType-by-year")
+    public ResponseEntity<?> statisticByLocationOrVehicleTypeYear(
+            @RequestParam(required = false, value = "location") String location,
+            @RequestParam(required = false, value = "vehicle_type") String vehicleTypeName,
+            @RequestParam("year") List<Integer> years) throws ApplicationException {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                vehicleRentalStatisticService.statisticFromLocationOrVehicleTypeByYear(location, vehicleTypeName, years));
     }
+
     @GetMapping("/statistic-by-date")
     public ResponseEntity<?> statisticByDate(@RequestParam(required = false,value = "location")String location,
                                              @RequestParam(required = false,value = "vehicle_type")String vehicleTypeName,
